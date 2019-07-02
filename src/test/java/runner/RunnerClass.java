87d05ddf2e5f608a5cfb81f0aa71d66ad2beb918
JerlinSelvan <52442302+JerlinSelvan@users.noreply.github.com>
2019-07-02 16:22:15 +0530

    Update RunnerClass.java

--- src/test/java/runner/RunnerClass.java
+++ src/test/java/runner/RunnerClass.java
@@ -8,7 +8,8 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features={"src/main/resources/jerlinEg5.feature"},
                    glue="stepdef",
-                   plugin="html:target/htmlreport")
+                  plugin="json:target/jsonreport.json")
+                   //plugin="html:target/htmlreport")
                 		  // plugin="json:target/cucumber.json")
                 		  // plugin="junit:target/xmlreport.xml",
                 		   //tags={"@smoke"})
